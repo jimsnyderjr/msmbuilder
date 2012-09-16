cc0a9620b4743b3b1038b24fe112ce8edd58170e
Robert McGibbon <rmcgibbo@gmail.com>
2012-09-16 04:05:57 -0700

    took get() out of context and just made it module level

--- src/python/testing.py
+++ src/python/testing.py
@@ -9,7 +9,7 @@ from nose.tools import ok_, eq_
 
 from pkg_resources import resource_filename
 
-__all__ = ['context', 'assert_traj_equal', 'assert_spase_matrix_equal',
+__all__ = ['get', 'assert_traj_equal', 'assert_spase_matrix_equal',
            # stuff that was imported from numpy / nose too
           'ok_', 'eq_', 'assert_allclose', 'assert_almost_equal',
           'assert_approx_equal', 'assert_array_almost_equal',
@@ -17,73 +17,67 @@ __all__ = ['context', 'assert_traj_equal', 'assert_spase_matrix_equal',
           'assert_array_less', 'assert_array_max_ulp', 'assert_equal',
           'assert_raises', 'assert_string_equal', 'assert_warns']
 
-class Context(object):
-    def get(self, key, load=True):
-        """"
-        Load up a resource for testing
+def get(self, key, load=True):
+    """"
+    Load up a resource for testing
+    
+    Parameters
+    ----------
+    key : string
+        name of the resource, ususally a filename or path
+    attempt_load : bool, optional
+        if true, we attempt to load the resource from disk using a set
+        of heuristics to determine what method to load the file with
         
-        Parameters
-        ----------
-        key : string
-            name of the resource, ususally a filename or path
-        attempt_load : bool, optional
-            if true, we attempt to load the resource from disk using a set
-            of heuristics to determine what method to load the file with
-            
-        Notes
-        -----
-        The heuristic basically just looks at the filename extension, but it
-        has a few tricks, like loading AtomIndices ith dtype=np.int
+    Notes
+    -----
+    The heuristic basically just looks at the filename extension, but it
+    has a few tricks, like loading AtomIndices ith dtype=np.int
+    
+    """
+    # delay these imports, since this module is loaded in a bunch
+    # of places but not necessarily used
+    import scipy.io
+    from msmbuilder import Trajectory, Serializer
+    
+    # ReferenceData is where all the data is stored
+    fn = resource_filename('msmbuilder', os.path.join('ReferenceData', key))
+    
+    # the filename extension
+    ext = os.path.splitext(fn)[1]
+    
+    if not load:
+        val = fn
+    
+    # load trajectories
+    elif ext in ['.lh5', '.pdb']:
+        val = Trajectory.LoadTrajectoryFile(fn)
         
-        """
-        # delay these imports, since this module is loaded in a bunch
-        # of places but not necessarily used
-        import scipy.io
-        from msmbuilder import Trajectory, Serializer
+    # load flat text files
+    elif 'AtomIndices.dat' in fn:
+        # try loading AtomIndices first, because the default for loadtxt
+        # is to use floats
+        val = np.loadtxt(fn, dtype=np.int)
+    elif ext in ['.dat']:
+        # try loading general .dats with floats
+        val = np.loadtxt(fn)
         
-        # ReferenceData is where all the data is stored
-        fn = resource_filename('msmbuilder', os.path.join('ReferenceData', key))
+    # load with serializer files that end with .h5, .hdf or .h5.distances
+    elif ext in ['.h5', '.hdf']:
+        val = Serializer.LoadFromHDF(fn)
+    elif fn.endswith('.h5.distances'):
+         val = Serializer.LoadFromHDF(fn)
+    
+    # load matricies
+    elif ext in ['.mtx']:
         
-        # the filename extension
-        ext = os.path.splitext(fn)[1]
-        
-        if not load:
-            val = fn
-        
-        # load trajectories
-        elif ext in ['.lh5', '.pdb']:
-            val = Trajectory.LoadTrajectoryFile(fn)
-            
-        # load flat text files
-        elif 'AtomIndices.dat' in fn:
-            # try loading AtomIndices first, because the default for loadtxt
-            # is to use floats
-            val = np.loadtxt(fn, dtype=np.int)
-        elif ext in ['.dat']:
-            # try loading general .dats with floats
-            val = np.loadtxt(fn)
-            
-        # load with serializer files that end with .h5, .hdf or .h5.distances
-        elif ext in ['.h5', '.hdf']:
-            val = Serializer.LoadFromHDF(fn)
-        elif fn.endswith('.h5.distances'):
-             val = Serializer.LoadFromHDF(fn)
-        
-        # load matricies
-        elif ext in ['.mtx']:
-            
-            val = scipy.io.mmread(fn)
-        else:
-            raise TypeError("I could not infer how to load this file. You "
-                "can either request load=False, or perhaps add more logic to "
-                "the load heuristics in this class: %s" % fn)
-        
-        return val
-    def __getitem__(self, key):
-        "Alias for get()"
-        return self.get(key)
-
-context = Context()
+        val = scipy.io.mmread(fn)
+    else:
+        raise TypeError("I could not infer how to load this file. You "
+            "can either request load=False, or perhaps add more logic to "
+            "the load heuristics in this class: %s" % fn)
+    
+    return val
 
 def assert_traj_equal(t1, t2):
     """Assert two msmbuilder trajectories are equal. This method should actually
